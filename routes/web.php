fda591e8a0f170019f953d35bda3a045700e62b8
sidiropulometin@gmail.com <sidiropulometin@gmail.com>
2023-12-21 22:19:43 +0600

    Маршрутизация, добавили основные routes 21.12.2023

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\WalletController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'test2';
-});
+
+
+Route::get('/', [MainController::class,'index'])->name('main.index');
+Route::get('/currency_edit', [MainController::class,'currency_edit'])->name('main.currency_edit');//update: post
+Route::put('/currency_edit', [MainController::class,'currency_update'])->name('main.currency_update');//update: post
+Route::get('/categories', [MainController::class,'categories'])->name('main.categories');
+
+Route::get('/incomes_category_create', [MainController::class,'incomes_create'])->name('main.incomes_create');//save: post
+Route::get('/outcomes_category_create', [MainController::class,'outcomes_create'])->name('main.outcomes_create');
+Route::post('/incomes_category_create', [MainController::class,'incomes_save'])->name('main.incomes_save');//save: post
+Route::post('/outcomes_category_create', [MainController::class,'outcomes_save'])->name('main.outcomes_save');
+
+
+Route::post('/income_random_create/{wallet_id}',[WalletController::class,'save_random_income'])->name('wallet.save_random_income');//save random income
+Route::post('/outcome_random_create/{wallet_id}',[WalletController::class,'save_random_outcome'])->name('wallet.save_random_outcome');//save random outcome
+
+
+
+
+Route::get('/wallet/{id}', [WalletController::class,'wallet_main'])->name('wallet.main');
+Route::get('/transfer_create', [WalletController::class,'transfer_create'])->name('wallet.transfer_create');
+Route::post('/transfer_create',[WalletController::class,'transfer_save'])->name('wallet.transfer_save');
+Route::get('/wallet/create',[WalletController::class,'create_wallet'])->name('wallet.create');//save random income
+Route::post('/wallet/create',[WalletController::class,'save_wallet'])->name('wallet.save');
+
+
+
+// Route::post('/posts/{post_id?}/attach_tag/',[PostController::class,'attach_tag'])->name('posts.attach_tag');
+
+
+
