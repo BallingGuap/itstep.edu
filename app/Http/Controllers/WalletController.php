13744d06860931bc632219d533c6cdb5e70379cf
sidiropulometin@gmail.com <sidiropulometin@gmail.com>
2023-12-22 21:38:21 +0600

    Завершил все функции для get запросов, создал blade шаблоны для их view

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -4,8 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use App\Models\{Currency, Wallet};
 
 class WalletController extends Controller
 {
-    //
+    public function create_wallet()
+    {
+        $currencies = Currency::all();
+        return view('TEMPLATE', compact('$currencies'));
+    }
+
+    public function wallet_main($id)
+    {
+        $wallet = Wallet::findOrFail($id);
+        $currencies = Currency::all(); 
+        return view('TEMPLATE', compact('currencies')); 
+    }
+
+    public function transfer_create()
+    {
+        $wallets = Wallet::all(); 
+        return view('TEMPLATE', compact('wallets')); 
+    }
 }
