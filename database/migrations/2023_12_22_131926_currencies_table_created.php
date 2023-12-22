5c67750efd2e71655368d710b7730d7bdb4da06b
Sergey <mr.ultimateground@gmail.com>
2023-12-22 19:21:38 +0600

    currency migration added

---
+++ database/migrations/2023_12_22_131926_currencies_table_created.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CurrenciesTableCreated extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->double('exchangeRateToTenge');
+            $table->tinyText('symbol');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
