9240003ca0b7aa80f0a01087f4c51bb366eb8fd7
adelmnv <99648706+adelmnv@users.noreply.github.com>
2023-12-26 20:19:30 +0600

    view кошелька

--- resources/views/wallet/index.blade.php
+++ resources/views/wallet/index.blade.php
@@ -1,42 +1,46 @@
 @extends('app') 
 
+
 @section('content')
-<div class="flex  justify-center h-screen">
-    <div class="bg-white p-8">
-        <div class="mb-8">
-            <ul class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                <li class="bg-blue-100 p-6 rounded-md shadow-md text-center">
-                    <span class="font-bold text-lg">{{ $wallet->name }}</span> 
+<div class="container mx-auto bg-white p-8 mt-8">
+        <h1 class="text-3xl font-bold mb-6 text-center">Данные о кошельке</h1>
+
+        <div class="mb-8 ">
+            <div class="w-1/3 mx-auto">
+                <div class="bg-blue-100 p-6 rounded-md shadow-md text-center">
+                    <span class="font-bold text-lg">{{ $wallet->name }}</span>
                     <p class="text-gray-600 mb-4">Валюта кошелька: {{ $wallet->currency->symbol }}</p>
                     <p class="text-gray-600 mb-4">Баланс кошелька: {{ $wallet->balance }}</p>
-                    <a href="{{ route('wallet.transfer_create', ['current_wallet_id' => $wallet->id]) }}" class="block bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300 mb-2">Перевести средства</a>
+                    <a href="{{ route('wallet.transfer_create', ['current_wallet_id' => $wallet->id]) }}" class="block bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300 mb-2 w-1/2 mx-auto">Перевести средства</a>
                     <form action="{{ route('wallet.save_random_income', ['wallet_id' => $wallet->id]) }}" method="POST" style="display:inline;">
                         @csrf
                         <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded-md ml-2 hover:bg-green-700 transition duration-300">Генерировать доход</button>
                     </form>
-                    
-                    <!-- Форма для генерации случайного расхода -->
                     <form action="{{ route('wallet.save_random_outcome', ['wallet_id' => $wallet->id]) }}" method="POST" style="display:inline;">
                         @csrf
                         <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded-md ml-2 hover:bg-red-700 transition duration-300">Генерировать расход</button>
                     </form>
-                </li>
-            </ul>
-            <div class="row">
-                <div class="card chart-container col-4">
-                    <canvas id="chart_pie"></canvas>
                 </div>
-                <div class="card chart-container col-4">
-                    <canvas id="chart_bar_1"></canvas>
-                </div>
-                <div class="card chart-container col-4">
-                    <canvas id="chart_bar_2"></canvas>
+            </div>
+
+            <div class="mt-8 mb-8">
+                <h2 class="text-2xl font-bold mb-4 text-center">Диаграммы</h2>
+                <div class="mx-auto row">
+                    <div class="card chart-container mx-auto col-4">
+                        <canvas id="chart_pie"></canvas>
+                    </div>
+                    <div class="card chart-container col-4 mx-auto">
+                        <canvas id="chart_bar_2"></canvas>
+                    </div>
+                    <div class="card chart-container  col-4 mx-auto">
+                        <canvas id="chart_bar_1"></canvas>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
+
 
 @push('chart-scripts')
 <script>
